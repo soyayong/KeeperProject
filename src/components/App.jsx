59b96cc5ddb10462326ba7b2a605b289cb64d6be
soyayong <45891158+soyayong@users.noreply.github.com>
2021-05-04 21:23:04 +0800

    Update App scritps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,33 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const removeWatermark = () => {
+    const ids = [];
+    const iframes = document.body.querySelectorAll("iframe");
+    for (const iframe of iframes) {
+      if (iframe.id.startsWith("sb__open-sandbox")) ids.push(iframe.id);
+    }
+    for (const id of ids) {
+      const node = document.createElement("div");
+      node.style.setProperty("display", "none", "important");
+      node.id = id;
+      document.getElementById(id).remove();
+      document.body.appendChild(node);
+    }
+  };
+
+  setTimeout(removeWatermark, 1000);
+
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return [...prevNotes, newNote];
     });
   }
 
   function deleteNote(id) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
